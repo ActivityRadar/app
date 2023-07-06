9b5f2820232cd5fdbc6dad410c157bf91c9e197a
Suriija <svwebung@outlook.de>
2023-07-06 13:36:44 +0200

    Details Screen and onTap to Details Screen

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,11 @@
+import 'package:app/constants/contants.dart';
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black26,
+    );
+  }
+}
--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-
+import 'package:app/screens/details_screen.dart';
+import 'package:app/widgets/details_short.dart';
 import 'package:app/constants/contants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -58,37 +59,63 @@ class BuildContainer extends StatelessWidget {
         height: 130.0,
         child: ListView(
           scrollDirection: Axis.horizontal,
-          children: const <Widget>[
-            SizedBox(width: 10.0),
+          children: <Widget>[
+            const SizedBox(width: 10.0),
             Padding(
-              padding: EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
               child: BoxesDetails(
-                  imageurl:
-                      "https://cdn.pixabay.com/photo/2016/02/18/23/27/table-tennis-1208383_960_720.jpg",
-                  lat: 13.3086758,
-                  long: 52.5748306,
-                  titel: "Tischtennis"),
+                imageurl:
+                    "https://cdn.pixabay.com/photo/2016/02/18/23/27/table-tennis-1208383_960_720.jpg",
+                lat: 13.3086758,
+                long: 52.5748306,
+                titel: "Tischtennis",
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailsScreen(),
+                    ),
+                  );
+                },
+              ),
             ),
-            SizedBox(width: 10.0),
+            const SizedBox(width: 10.0),
             Padding(
-              padding: EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
               child: BoxesDetails(
-                  imageurl:
-                      "https://cdn.pixabay.com/photo/2014/11/29/17/49/playground-550535_960_720.jpg",
-                  lat: 13.4496164,
-                  long: 52.5317128,
-                  titel: "Tischtennis"),
+                imageurl:
+                    "https://cdn.pixabay.com/photo/2014/11/29/17/49/playground-550535_960_720.jpg",
+                lat: 13.4496164,
+                long: 52.5317128,
+                titel: "Tischtennis",
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailsScreen(),
+                    ),
+                  );
+                },
+              ),
             ),
-            SizedBox(width: 10.0),
+            const SizedBox(width: 10.0),
             Padding(
-              padding: EdgeInsets.all(8.0),
-              child: BoxesDetails(
+                padding: const EdgeInsets.all(8.0),
+                child: BoxesDetails(
                   imageurl:
                       "https://cdn.pixabay.com/photo/2016/09/05/23/28/blue-1648005_960_720.jpg",
                   lat: 13.4496164,
                   long: 52.5317128,
-                  titel: "Tischtennis"),
-            ),
+                  titel: "Tischtennis",
+                  press: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailsScreen(),
+                      ),
+                    );
+                  },
+                )),
           ],
         ),
       ),
@@ -122,6 +149,7 @@ class _ActivityMarkerMapState extends State<ActivityMarkerMap> {
 
   void onMapEvent(MapEvent event) {
     if (event is! MapEventMoveEnd) return;
+    // ignore_for_file: avoid_print
     print("move triggered");
     performSearch();
   }
@@ -130,7 +158,6 @@ class _ActivityMarkerMapState extends State<ActivityMarkerMap> {
     bounds = widget.mapController.bounds!;
     print(
         '${bounds.south}, ${bounds.west}, ${bounds.east}, ${widget.mapState.activity.value}');
-
     print("fetch locations started");
     List<Marker> markers_ =
         await fetchLocations(bounds, widget.mapState.activity.value);
@@ -241,141 +268,6 @@ class SearchBar extends StatelessWidget {
           ))
         ],
       ),
-    );
-  }
-}
-
-class BoxesDetails extends StatelessWidget {
-  const BoxesDetails({
-    super.key,
-    required this.imageurl,
-    required this.lat,
-    required this.long,
-    required this.titel,
-  });
-
-  final String imageurl;
-  final double lat;
-  final double long;
-  final String titel;
-
-  @override
-  Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    var height = size.height;
-    var width = size.width;
-    return GestureDetector(
-      onTap: () {},
-      child: new FittedBox(
-        child: Material(
-          color: Colors.white,
-          elevation: 14.0,
-          borderRadius: BorderRadius.circular(24.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              SizedBox(
-                width: 180,
-                height: 200,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(24.0),
-                  child: new Image(
-                    fit: BoxFit.fill,
-                    image: NetworkImage(imageurl),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: DetailContainer(titel: titel),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  myDetailsContainer({required id}) {}
-}
-
-class DetailContainer extends StatelessWidget {
-  const DetailContainer({
-    super.key,
-    required this.titel,
-  });
-
-  final String titel;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Text(
-            titel,
-            style: const TextStyle(
-                color: DesignColors.naviColor,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold),
-          ),
-        ),
-        const SizedBox(height: 5.0),
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Text(
-              "4.1",
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: 18.0,
-              ),
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.amber,
-              size: 15.0,
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.amber,
-              size: 15.0,
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.amber,
-              size: 15.0,
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.amber,
-              size: 15.0,
-            ),
-            Icon(
-              Icons.star_half_sharp,
-              color: Colors.amber,
-              size: 15.0,
-            ),
-            Text(
-              "(243)",
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: 18.0,
-              ),
-            )
-          ],
-        ),
-        const SizedBox(height: 5.0),
-        const Text(
-          "KM entfernt",
-          style: TextStyle(
-              color: Colors.black54,
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold),
-        )
-      ],
     );
   }
 }
