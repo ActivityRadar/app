dad5e12ab8efd545529c4bbae7b2d8ecc9f1201d
Anton Kriese <anton.kriese@gmx.de>
2023-08-02 18:05:23 +0200

    fix: increase collapsed height vs pixel overlfow

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -11,7 +11,7 @@ class SettingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const double collapsedHeight = 60;
+    const double collapsedHeight = 90;
     const double expandedHeight = 160;
 
     return Scaffold(
