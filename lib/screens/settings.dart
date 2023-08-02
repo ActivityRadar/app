01860fa0988935846b1b7c8c32aa090ebf026a61
Anton Kriese <anton.kriese@gmx.de>
2023-08-02 18:08:04 +0200

    Add Setting Page

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,36 +1,356 @@
 import 'package:app/constants/contants.dart';
-import 'package:app/provider/photos.dart';
+import 'package:app/screens/setting_password.dart';
+import 'package:app/screens/settings_email.dart';
+import 'package:app/screens/settings_name.dart';
+import 'package:app/screens/settings_privacy.dart';
+
 import 'package:flutter/material.dart';
 
 class SettingScreen extends StatelessWidget {
-  SettingScreen({Key? key}) : super(key: key);
+  const SettingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    const double collapsedHeight = 60;
+    const double expandedHeight = 160;
+
     return Scaffold(
-        body: Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          IconButton(
-              icon: const Icon(Icons.arrow_drop_down),
-              onPressed: () async {
-                const String photoLink = "logo.png";
-                MemoryImage photo =
-                    await PhotoManager.instance.getPhoto(photoLink);
-                if (context.mounted) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          PhotoShower(image: photo, photoName: photoLink),
+      body: CustomScrollView(slivers: <Widget>[
+        SliverAppBar(
+          expandedHeight: expandedHeight,
+          collapsedHeight: collapsedHeight,
+          pinned: true,
+          backgroundColor: DesignColors.kBackgroundColor,
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle:
+                    const TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {},
+              child: const Text('loggout'),
+            ),
+          ],
+          flexibleSpace: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              return FlexibleSpaceBar(
+                //ToDo error beim scrollen
+                titlePadding: const EdgeInsets.all(16.0),
+                title: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Stack(alignment: Alignment.center, children: [
+                      const CircleAvatar(
+                        backgroundImage:
+                            AssetImage('assets/locationPhotoPlaceholder.jpg'),
+                        radius: 40,
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const DisplayNameSwitch(),
+                              ),
+                            );
+                          },
+                          child: const CircleAvatar(
+                              radius: 8,
+                              child: Center(
+                                child: Icon(
+                                  Icons.edit,
+                                  size: 8,
+                                ),
+                              )),
+                        ),
+                      ),
+                    ]),
+                  ],
+                ),
+                centerTitle: true,
+              );
+            },
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(top: 8, left: 15, bottom: 4),
+                  child: Text(
+                    "Konto",
+                  ),
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(
+                      color: Color.fromARGB(51, 241, 241, 241),
                     ),
-                  );
-                }
-              }),
-        ],
-      ),
-    ));
+                    borderRadius: BorderRadius.circular(radiusborda),
+                  ),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(radiusborda),
+                                topRight: Radius.circular(radiusborda))),
+                        tileColor: Colors.white,
+                        title: const Text('Username and Displayname'),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const DisplayNameSwitch(),
+                            ),
+                          );
+                        },
+                      ),
+                      const Divider(height: 0),
+                      ListTile(
+                        tileColor: Colors.white,
+                        title: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text('Displayname'),
+                            Text(
+                              'Max Mustermann',
+                              style: TextStyle(color: Colors.black26),
+                            ),
+                          ],
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const DisplayNameSwitch(),
+                            ),
+                          );
+                        },
+                      ),
+                      const Divider(height: 0),
+                      ListTile(
+                        tileColor: Colors.white,
+                        title: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text('E-Mail'),
+                            Text(
+                              'max@mustermann.de',
+                              style: TextStyle(color: Colors.black26),
+                            ),
+                          ],
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const EmailSwitch(),
+                            ),
+                          );
+                        },
+                      ),
+                      const Divider(height: 0),
+                      ListTile(
+                        tileColor: Colors.white,
+                        title: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text('Password'),
+                          ],
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const PasswordSwitch(),
+                            ),
+                          );
+                        },
+                      ),
+                      const Divider(height: 0),
+                      ListTile(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(radiusborda),
+                                bottomRight: Radius.circular(radiusborda))),
+                        tileColor: Colors.white,
+                        title: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text('Privacy'),
+                          ],
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const PrivacySettingPage(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 8, left: 15, bottom: 4),
+                  child: Text(
+                    "App",
+                  ),
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(
+                      color: Color.fromARGB(51, 241, 241, 241),
+                    ),
+                    borderRadius: BorderRadius.circular(radiusborda),
+                  ),
+                  child: Column(children: [
+                    ListTile(
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(radiusborda),
+                              topRight: Radius.circular(radiusborda))),
+                      tileColor: Colors.white,
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          const Text('Language'),
+                          // Abstand zwischen Avatar und Text
+
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              textStyle: const TextStyle(color: Colors.black12),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'English',
+                              style: TextStyle(color: Colors.black12),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Divider(height: 0),
+                    const ListTile(
+                      tileColor: Colors.white,
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Map'),
+                        ],
+                      ),
+                    ),
+                    const Divider(height: 0),
+                    const ListTile(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(radiusborda),
+                              bottomRight: Radius.circular(radiusborda))),
+                      tileColor: Colors.white,
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Farbe'),
+                        ],
+                      ),
+                    ),
+                  ]),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 8, left: 15, bottom: 4),
+                  child: Text(
+                    "Rechtliche",
+                  ),
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(
+                      color: Color.fromARGB(51, 241, 241, 241),
+                    ),
+                    borderRadius: BorderRadius.circular(radiusborda),
+                  ),
+                  child: const Column(
+                    children: [
+                      ListTile(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(radiusborda),
+                                topLeft: Radius.circular(radiusborda))),
+                        tileColor: Colors.white,
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text('data protection'),
+                          ],
+                        ),
+                      ),
+                      Divider(height: 0),
+                      ListTile(
+                        tileColor: Colors.white,
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text('AGB'),
+                          ],
+                        ),
+                      ),
+                      Divider(height: 0),
+                      ListTile(
+                        tileColor: Colors.white,
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text('Impressum'),
+                          ],
+                        ),
+                      ),
+                      Divider(height: 0),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text("App-Version: Beta",
+                    style: TextStyle(color: Colors.black45)),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(
+                        fontSize: 13, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {},
+                  child: const Text('loggout'),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(
+                        fontSize: 13, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Konto löschen'),
+                ),
+              ],
+            ),
+            Padding(
+                padding: const EdgeInsets.only(top: 8, left: 15, bottom: 4),
+                child: Container(
+                  height: 70.0,
+                )),
+          ]),
+        )
+      ]),
+    );
   }
 }
 
@@ -53,7 +373,7 @@ class PhotoShower extends StatelessWidget {
               onPressed: () {
                 Navigator.pop(context);
               }),
-          actions: <Widget>[]),
+          actions: const <Widget>[]),
       body: Image(image: image),
     );
   }
---
+++ lib/screens/settings_email.dart
@@ -0,0 +1,105 @@
+import 'package:app/constants/contants.dart';
+import 'package:flutter/material.dart';
+
+class EmailSwitch extends StatelessWidget {
+  const EmailSwitch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final formEmailKey = GlobalKey<FormState>();
+
+    TextEditingController emailController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: TextButton(
+          style: TextButton.styleFrom(
+            textStyle:
+                const TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+            ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(content: Text('Cancel')));
+          },
+          child: const Text(
+            'Cancel',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        actions: [
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle:
+                  const TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+            ),
+            onPressed: () {
+              if (formEmailKey.currentState!.validate()) {
+                Navigator.pop(context);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Please fill input')),
+                );
+              }
+            },
+            child: const Text(
+              'Finish',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+      body: Form(
+        key: formEmailKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedErrorBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red),
+                      ),
+                      errorBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      labelText: "email"),
+                  validator: (value) {
+                    if (value != null || value!.isEmpty) {
+                      if (!RegExps.email.hasMatch(value)) {
+                        return "Email is wrong";
+                      }
+                    }
+
+                    return null;
+                  },
+                ),
+              ),
+              const Padding(
+                  padding: EdgeInsets.only(left: 8),
+                  child: Text(
+                    "Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin Moin",
+                    style: TextStyle(color: Colors.black12),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
