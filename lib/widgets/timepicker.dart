75b72ac641b773d13ebf196651949eaed3c6a435
Suriija <svwebung@outlook.de>
2023-08-08 15:50:14 +0200

    feat: Time and Date picker

---
+++ lib/widgets/timepicker.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateTimePicker extends StatefulWidget {
+  const DateTimePicker({super.key});
+
+  @override
+  _DateTimePickerState createState() => _DateTimePickerState();
+}
+
+class _DateTimePickerState extends State<DateTimePicker> {
+  DateTime selectedDate = DateTime.now();
+  TimeOfDay selectedTime = TimeOfDay.now();
+  final DateTime today = DateTime.now();
+
+  Future<void> _selectDate() async {
+    final DateTime picked = await showDatePicker(
+          context: context,
+          initialDate: selectedDate,
+          firstDate: today,
+          lastDate: today.add(const Duration(days: 14)),
+        ) ??
+        selectedDate;
+    if (picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  Future<void> _selectTime() async {
+    final TimeOfDay picked = await showTimePicker(
+          context: context,
+          initialTime: selectedTime,
+        ) ??
+        selectedTime;
+    if (picked != selectedTime) {
+      setState(() {
+        selectedTime = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String formattedDate = DateFormat('dd.MM.yyyy').format(selectedDate);
+    String formattedTime =
+        '${selectedTime.hour.toString().padLeft(2, '0')}:${selectedTime.minute.toString().padLeft(2, '0')}';
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ListTile(
+          title: Text("Datum: $formattedDate"),
+          trailing: const Icon(Icons.calendar_today),
+          onTap: _selectDate,
+        ),
+        ListTile(
+          title: Text("Uhrzeit: $formattedTime"),
+          trailing: const Icon(Icons.access_time),
+          onTap: _selectTime,
+        ),
+      ],
+    );
+  }
+}
