911d4de797f129c7e7f82f82aad2b1ca7ddaa684
Anton Kriese <anton.kriese@gmx.de>
2023-08-14 12:42:39 +0200

    feat: regex: remove capital letters from username
    also, allow shorter names (from 3 up to 18 in total)
    also, dont start username with a digit

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -26,8 +26,12 @@ Map<String, String> shortSports = {
 class RegExps {
 //RegExp
   static RegExp displayname = RegExp(r'^[A-Za-züöä\s]+$');
-  static RegExp username = RegExp(
-      r'[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]');
+
+  // start with a letter, then between 1 and 16 chars ., _, or letter/digits, where
+  // a . is not followed by another . or a _ or vice versa.
+  // at the end a letter or digit
+  static RegExp username =
+      RegExp(r'^[a-z](_(?!(\.|_))|\.(?!(_|\.))|[a-z0-9]){1,16}[a-z0-9]$');
 
   static RegExp password =
       RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$');
