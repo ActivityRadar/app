e99352c31f3b402fb411b390ef9d2275ab740952
Anton Kriese <anton.kriese@gmx.de>
2023-08-16 10:30:56 +0200

    feat: save and load appstate to share_prefs
    only on app life cycle events (detached, paused, startup)

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,8 +1,18 @@
+import 'dart:convert';
+
+import 'package:app/model/functions.dart';
 import 'package:app/model/generated.dart';
 import 'package:app/provider/generated/users_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StorageKeys {
+  static const String userInfo = "userInfo";
+  static const String boundsSouthWest = "mapBoundsSW";
+  static const String boundsNorthEast = "mapBoundsNE";
+}
 
 class AppState extends ChangeNotifier {
   UserDetailed? _currentUser;
@@ -39,5 +49,38 @@ class AppState extends ChangeNotifier {
 
   bool get isAllowedToCreateLocation {
     return currentUser == null ? false : (currentUser!.trustScore >= 100);
+  }
+
+  void loadFromStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    if (prefs.containsKey(StorageKeys.boundsSouthWest)) {
+      LatLng southWest = toLatLng(GeoJsonLocation.fromJson(
+          jsonDecode(prefs.getString(StorageKeys.boundsSouthWest)!)));
+      LatLng northEast = toLatLng(GeoJsonLocation.fromJson(
+          jsonDecode(prefs.getString(StorageKeys.boundsNorthEast)!)));
+
+      _mapPosition = LatLngBounds(southWest, northEast);
+    }
+
+    if (prefs.containsKey(StorageKeys.userInfo)) {
+      _currentUser = UserDetailed.fromJson(
+          jsonDecode(prefs.getString(StorageKeys.userInfo)!));
+    }
+  }
+
+  void saveToStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (_currentUser != null) {
+      await prefs.setString(
+          StorageKeys.userInfo, jsonEncode(_currentUser!.toJson()));
+    } else {
+      await prefs.remove(StorageKeys.userInfo);
+    }
+
+    await prefs.setString(StorageKeys.boundsSouthWest,
+        jsonEncode(toLongLat(_mapPosition.southWest).toJson()));
+    await prefs.setString(StorageKeys.boundsNorthEast,
+        jsonEncode(toLongLat(_mapPosition.northEast).toJson()));
   }
 }
