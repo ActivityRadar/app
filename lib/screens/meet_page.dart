f2054b191b5d3a0e4b66919017de969ecec23856
Suriija <svwebung@outlook.de>
2023-08-19 14:53:00 +0200

    feat: Upgrade meet-Page

--- lib/screens/meet_page.dart
+++ lib/screens/meet_page.dart
@@ -1,5 +1,6 @@
 import 'package:app/constants/constants.dart';
 import 'package:app/constants/design.dart';
+import 'package:app/widgets/custom/card.dart';
 import 'package:app/widgets/custom/chip.dart';
 
 import 'package:app/widgets/custom/alertdialog.dart';
@@ -29,88 +30,137 @@ class MeetPage extends StatelessWidget {
               background: SizedBox(
                 height: 100,
                 child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(AppStyle
-                          .cornerRadius), // Radius der abgerundeten Ecken
-                    ),
-                    child: Stack(children: [
-                      const MeetMap(),
-                      Positioned(
-                          bottom:
-                              0, // Positioniert den Container am unteren Rand
-                          left: 0,
-                          right: 0,
-                          child: Container(
-                              alignment: Alignment.bottomCenter,
-                              child: const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.only(left: 12.0),
-                                    child: CustomChip(
-                                      text: "Table Tennis",
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(right: 12.0),
-                                    child: CustomChip(text: "ab 14 Uhr"),
-                                  )
-                                ],
-                              ))),
-                    ])),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                        AppStyle.cornerRadius), // Radius der abgerundeten Ecken
+                  ),
+                  child: MeetMap(),
+                ),
               ),
             ),
             actions: const [MeetPopupMenuCard()],
             iconTheme: const IconThemeData(
               color: DesignColors.naviColor,
             )),
+        SliverPersistentHeader(
+          delegate: MyHeaderDelegate(
+            child: Container(
+              color: DesignColors.kBackgroundColor,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(children: [
+                            Row(children: [
+                              const Padding(
+                                  padding: EdgeInsets.only(left: 10, top: 60)),
+                              const CircleAvatar(
+                                backgroundImage: AssetImages.avatarEmpty,
+                                radius: 20,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              MediumText(
+                                text: 'Max Mustermann',
+                                width: width,
+                              ),
+                            ]),
+                          ]),
+                          CustomTextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop(); // Dialog schließen
+                              },
+                              text: 'Anfrage senden'),
+                        ]),
+                    Center(
+                      child: TitleText(
+                        text: "Table Tennis",
+                        width: width,
+                      ),
+                    ),
+                  ]),
+            ),
+          ),
+
+          pinned: true, // Nur dieser Header wird gepinnt
+        ),
         SliverList(
           delegate: SliverChildListDelegate([
+            Padding(
+              padding: const EdgeInsets.only(left: 9.0, top: 10.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CardPeople(
+                        people: '1-4',
+                      ),
+                      CardEvent(date: "14.02.13"),
+                      CardSchedule(
+                        time: "14 Uhr",
+                      ),
+                      CardPublic(),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
             Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(children: [
-                          Row(children: [
-                            const Padding(
-                                padding: EdgeInsets.only(left: 10, top: 60)),
-                            const CircleAvatar(
-                              backgroundImage: AssetImages.avatarEmpty,
-                              radius: 20,
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            MediumText(
-                              text: 'Max Mustermann',
-                              width: width,
-                            ),
-                          ]),
-                        ]),
-                        CustomTextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop(); // Dialog schließen
-                            },
-                            text: 'Anfrage senden'),
-                      ]),
-                  Center(
-                    child: TitleText(
-                      text: "Table Tennis",
-                      width: width,
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.only(left: 9.0, top: 10.0),
+                  SingleChildScrollView(
+                      scrollDirection:
+                          Axis.horizontal, // Horizontales Scrollen aktivieren
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          TitleText(text: "Time", width: width),
-                          TitleText(text: '11:00', width: width),
+                          Padding(
+                            padding: EdgeInsets.only(left: 12.0),
+                            child: CustomChip(
+                              text: "Table Tennis",
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 12.0),
+                            child: CustomChip(
+                              text: "Tennis",
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 12.0),
+                            child: CustomChip(
+                              text: "Swimming",
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 12.0),
+                            child: CustomChip(
+                              text: "Football",
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 12.0),
+                            child: CustomChip(
+                              text: "soccer",
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 12.0),
+                            child: CustomChip(
+                              text: "Volleyball",
+                            ),
+                          ),
                         ],
                       )),
+                  SizedBox(
+                    height: 10,
+                  ),
                   Padding(
                     padding: const EdgeInsets.only(left: 9.0, top: 10.0),
                     child: TitleText(text: "description", width: width),
@@ -121,10 +171,161 @@ class MeetPage extends StatelessWidget {
                           text:
                               "Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, Moin, ",
                           width: width)),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 9.0, top: 10.0),
+                    child: TitleText(text: "Sind auch dabei:", width: width),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.all(9.0),
+                      child: Column(
+                        children: [
+                          ProfilListeCardView(
+                              width: width, name: 'Max Mustermann'),
+                          Divider(
+                            // Hier wird die Trennlinie hinzugefügt
+                            color: const Color.fromARGB(
+                                63, 0, 0, 0), // Farbe der Trennlinie
+                          ),
+                          ProfilListeCardView(
+                              width: width, name: 'Max Mustermann'),
+                          Divider(
+                            // Hier wird die Trennlinie hinzugefügt
+                            color: const Color.fromARGB(
+                                63, 0, 0, 0), // Farbe der Trennlinie
+                          ),
+                          ProfilListeCardView(
+                              width: width, name: 'Max Mustermann'),
+                        ],
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.all(9.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ButtonBookMark(),
+                          CustomElevatedButton(
+                              onPressed: () {}, text: "Anfrage senden")
+                        ],
+                      )),
+                  Padding(
+                      padding:
+                          const EdgeInsets.only(top: 8, left: 15, bottom: 4),
+                      child: Container(
+                        height: 70.0,
+                      )),
                 ]),
           ]),
         )
       ]),
+    );
+  }
+}
+
+class ProfilListeCardView extends StatelessWidget {
+  const ProfilListeCardView({
+    super.key,
+    required this.width,
+    required this.name,
+  });
+
+  final double width;
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(children: [
+        const Padding(padding: EdgeInsets.only(left: 10, top: 60)),
+        const CircleAvatar(
+          backgroundImage: AssetImages.avatarEmpty,
+          radius: 15,
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        MediumText(
+          text: name,
+          width: width,
+        ),
+      ]),
+    ]);
+  }
+}
+
+class CardPeople extends StatelessWidget {
+  const CardPeople({
+    super.key,
+    required this.people,
+  });
+
+  final String people;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 9.0),
+      child: Column(
+        children: [Icon(Icons.person), SmallText(text: people)],
+      ),
+    );
+  }
+}
+
+class CardPublic extends StatelessWidget {
+  const CardPublic({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Icon(Icons.public), SmallText(text: "Öffenlicht")],
+    );
+  }
+}
+
+class CardPrivat extends StatelessWidget {
+  const CardPrivat({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Icon(Icons.lock), SmallText(text: "nur Freunde")],
+    );
+  }
+}
+
+class CardSchedule extends StatelessWidget {
+  const CardSchedule({
+    super.key,
+    required this.time,
+  });
+
+  final String time;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Icon(Icons.schedule), SmallText(text: time)],
+    );
+  }
+}
+
+class CardEvent extends StatelessWidget {
+  const CardEvent({
+    super.key,
+    required this.date,
+  });
+
+  final String date;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(Icons.event),
+        SmallText(
+          text: date,
+        )
+      ],
     );
   }
 }
@@ -180,4 +381,27 @@ void _showDialog(BuildContext context) {
           });
     },
   );
+}
+
+class MyHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final Widget child;
+
+  MyHeaderDelegate({required this.child});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return child;
+  }
+
+  @override
+  double get maxExtent => 100.0;
+
+  @override
+  double get minExtent => 100.0;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
 }
