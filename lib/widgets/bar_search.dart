1a048df0bdc601b9187aaf2308ed695d3d0f39f1
Anton Kriese <anton.kriese@gmx.de>
2023-08-21 17:21:55 +0200

    feat: chip slider below search bar
    for quick selection of the searched activity type. Pressing a chip,
    directly makes the map search for that activity type and enters the word
    into the search bar

--- lib/widgets/bar_search.dart
+++ lib/widgets/bar_search.dart
@@ -1,20 +1,43 @@
 import 'package:app/constants/design.dart';
+import 'package:app/provider/activity_type.dart';
 import 'package:app/screens/map.dart';
+import 'package:app/widgets/activityType_short.dart';
 import 'package:app/widgets/bottomsheet.dart';
 import 'package:flutter/material.dart';
 
-class MapSearchBar extends StatelessWidget {
-  MapSearchBar({
+class MapSearchBar extends StatefulWidget {
+  const MapSearchBar({
     super.key,
     required this.mapState,
   });
 
   final MapScreenState mapState;
+
+  @override
+  State<MapSearchBar> createState() => _MapSearchBarState();
+}
+
+class _MapSearchBarState extends State<MapSearchBar> {
   TextEditingController textController = TextEditingController();
 
+  final ActivityManager activityManager = ActivityManager();
+
+  List<String> choices = [];
+
   void setActivity(String activity) {
-    mapState.activity.value = activity;
+    widget.mapState.activity.value = activityManager.getBackendType(activity);
     print('set activity to: $activity');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    textController.addListener(() {
+      setState(() {
+        choices = activityManager.searchInDisplayTypes(textController.text);
+      });
+    });
   }
 
   @override
@@ -23,31 +46,45 @@ class MapSearchBar extends StatelessWidget {
     var height = size.height;
     var width = size.width;
 
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(AppStyle.cornerRadiusSearch),
-      ),
-      child: TextField(
-        controller: textController,
-        decoration: InputDecoration(
-          prefixIcon: IconButton(
-            icon: Icon(Icons.filter_alt),
-            onPressed: () {
-              bottomSheetFilter(context);
-            },
+    return Column(
+      children: [
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(AppStyle.cornerRadiusSearch),
           ),
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              setActivity(textController.text);
-            },
+          child: TextField(
+            controller: textController,
+            decoration: InputDecoration(
+              prefixIcon: IconButton(
+                icon: const Icon(Icons.filter_alt),
+                onPressed: () {
+                  bottomSheetFilter(context);
+                },
+              ),
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  setActivity(textController.text);
+                },
+              ),
+              hintStyle: CustomTextStyle.hint,
+              hintText: "Search basketball, volleyball, table tennis ... ",
+            ),
+            textInputAction: TextInputAction.search,
+            onSubmitted: setActivity,
           ),
-          hintStyle: CustomTextStyle.hint,
-          hintText: "Search basketball, volleyball, table tennis ... ",
         ),
-        textInputAction: TextInputAction.search,
-        onSubmitted: setActivity,
-      ),
+        const SizedBox(height: 5),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: ActivityChipSlider(
+              activities: choices,
+              onPressed: (choice) {
+                textController.text = choice;
+                setActivity(choice);
+              }),
+        )
+      ],
     );
   }
 }
