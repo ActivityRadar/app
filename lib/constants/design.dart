a0dd41e92d73e1c9bae088d65541766873422e57
Suriija <svwebung@outlook.de>
2023-08-22 12:48:33 +0200

    add IconConstants

--- lib/constants/design.dart
+++ lib/constants/design.dart
@@ -38,3 +38,45 @@ class AppInputBorders {
     borderSide: BorderSide.none,
   );
 }
+
+class IconConstants {
+  static const IconData add = Icons.add;
+  static const IconData home = Icons.home;
+  static const IconData mapSharp = Icons.map_sharp;
+  static const IconData group = Icons.group;
+  static const IconData settings = Icons.settings;
+  static const IconData share = Icons.ios_share;
+  static const IconData addAPhoto = Icons.add_a_photo;
+  static const IconData editNote = Icons.edit_note;
+  static const IconData image = Icons.image;
+  static const IconData thumbUp = Icons.thumb_up;
+  static const IconData thumbDown = Icons.thumb_down;
+  static const IconData pushPin = Icons.push_pin;
+  static const IconData locationOn = Icons.location_on;
+  static const IconData person = Icons.person;
+  static const IconData public = Icons.public;
+  static const IconData lock = Icons.lock;
+  static const IconData schedule = Icons.schedule;
+  static const IconData event = Icons.event;
+  static const IconData calendarToday = Icons.calendar_today;
+  static const IconData keyboardBackspace = Icons.keyboard_backspace;
+  static const IconData icecream = Icons.icecream;
+  static const IconData upload = Icons.upload;
+  static const IconData filterAlt = Icons.filter_alt;
+  static const IconData search = Icons.search;
+  static const IconData star = Icons.star;
+  static const IconData gpsFixed = Icons.gps_fixed;
+  static const IconData gpsOff = Icons.gps_off;
+  static const IconData remove = Icons.remove;
+  static const IconData photo = Icons.photo;
+  static const IconData cameraAlt = Icons.camera_alt;
+  static const IconData favorite = Icons.favorite;
+  static const IconData accessTime = Icons.access_time;
+  static const IconData starHalfSharp = Icons.star_half_sharp;
+  static const IconData menu = Icons.menu;
+  static const IconData close = Icons.close;
+  static const IconData bookmarkAdded = Icons.bookmark_added;
+  static const IconData bookmark = Icons.bookmark;
+  static const IconData edit = Icons.edit;
+  static const IconData arrowBack = Icons.arrow_back;
+}
