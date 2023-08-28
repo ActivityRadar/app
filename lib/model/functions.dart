ac544895e9496ec792c7134ddea6af9837241117
Anton Kriese <anton.kriese@gmx.de>
2023-08-28 15:27:29 +0200

    feat: functions for formatting date and time
    if the date is in the next 7 days, the days names or e.g. "tomorrow" are
            displayed instead of the date

--- lib/model/functions.dart
+++ lib/model/functions.dart
@@ -1,6 +1,7 @@
 /// This file extends functionality for the data classes defined in ./generated/
 
 import 'package:app/provider/activity_type.dart';
+import 'package:intl/intl.dart';
 import 'package:latlong2/latlong.dart';
 
 import 'generated.dart';
@@ -27,6 +28,40 @@ String getTitle(LocationDetailedApi info) {
         .map((s) => ActivityManager.instance.getDisplayType(s))
         .join(", ");
   }
+}
+
+String getDateDescription(DateTime date) {
+  final today = DateTime.now();
+  if (date.isAfter(today.add(const Duration(days: 7)))) {
+    final formatter = DateFormat("dd.MM.yyyy");
+    return formatter.format(date);
+  }
+
+  final todayDate = DateTime(today.year, today.month, today.day);
+  final dateDate = DateTime(date.year, date.month, date.day);
+
+  var days = todayDate.difference(dateDate).inDays;
+  switch (days) {
+    case 0:
+      return "Heute";
+    case 1:
+      return "Morgen";
+    default:
+      return [
+        "Montag",
+        "Dienstag",
+        "Mittwoch",
+        "Donnerstag",
+        "Freitag",
+        "Samstag",
+        "Sonntag"
+      ][date.weekday - 1];
+  }
+}
+
+String formatMeetupTime(DateTime time) {
+  final DateFormat timeFormatter = DateFormat("hh:mm");
+  return "${timeFormatter.format(time)} Uhr";
 }
 
 LocationShortApi fromDetailed(LocationDetailedApi info) {
